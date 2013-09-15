b6a37a05cbeec5044c79d92ffd2e876979bc575b
j127 <hostelmarketing@gmail.com>
2013-09-15 10:30:31 -0700

    Last commit before pushing to github

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home pages" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+end
+
