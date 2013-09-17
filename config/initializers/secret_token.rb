4e157e8d37962e9069299708403e6936361bb793
j127 <hostelmarketing@gmail.com>
2013-09-17 11:15:32 -0700

    Dynamically generate secret key.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '2dd0adc5ec823cf21be2c50e1cda2f0384632231426368b29398d3a86daa5e34469930eae448e646323ea2c093b46b2c32524ee668d80717999edcdfc5d0e847'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
