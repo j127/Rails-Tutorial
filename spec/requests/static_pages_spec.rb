8ffe4ad4266e025fe54f8abc5ba818726e7ed2e4
j127 <hostelmarketing@gmail.com>
2013-09-17 11:55:01 -0700

    Section 3.5 exercise 1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,5 +38,17 @@ describe "StaticPages" do
     end
   end
 
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
+    end
+  end
+
 end
 
